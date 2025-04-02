063f540fcc4fe663106a4d997d33fdb20ea422ea
Leyton McKinney <leyton.mckinney@gmail.com>
2025-04-02 12:13:13 -0500

    Stop-and-Wait implemented in send.py

--- core/send.py
+++ core/send.py
@@ -1,8 +1,9 @@
 '''
 `send` is a module for sending UDP Datagrams.
 '''
-from socket import socket, AF_INET as ipv4, SOCK_DGRAM as connectionless, timeout
+from socket import socket, AF_INET as ipv4, SOCK_DGRAM as connectionless, timeout, inet_aton
 import os
+from time import sleep
 class SendUDP:
     # TODO: Write a concise description.
     '''
@@ -11,8 +12,8 @@ class SendUDP:
     # TODO: Does it make sense to have overloads for different parameter combinations?
     def __init__(self):
         # TODO: Add sensible defaults.
-        self._mtu = 200  # TODO: make toml config file and make this changeable there.
-        self._header_size = 0  # TODO: Add this to config file
+        self._mtu = 1500  # TODO: make toml config file and make this changeable there.
+        self._header_size = 10  # TODO: Add this to config file
         self.file_name = str()
         self.local_port = int()
         self.mode = int()
@@ -124,8 +125,7 @@ class SendUDP:
         self.timeout = time_out
         return True
     #endregion
-
-    #region core functionality
+    #region UDP
     def send_file(self) -> bool:
         '''
         Send a `file_name` to `receiver` using a UDP socket
@@ -134,35 +134,64 @@ class SendUDP:
             return False
 
         if self.get_mode() == 0:
-            self._send_file_stw()
+            return self._send_file_stw()
         elif self.get_mode() == 1:
-            self._send_file_sw()
+            return self._send_file_sw()
         else:
             return False
 
-    def _send_file_stw(self):
+    def _send_file_stw(self) -> bool:
         _timeout = float(self.timeout * 1e-3)  # milliseconds -> seconds
         sender = socket(ipv4, connectionless)
+        listening_ip = '127.0.0.1'
+        listening_port = self.get_localport()
+        sender.bind(("localhost", self.get_localport()))
         sender.settimeout(_timeout)
-        with open(self.get_filename()) as f:
-            data = f.read("")
-        packet_size = 
-        try:
-            sender.sendto(b'TEST', self.get_receiver())
-        # TODO: Figure out exceptions that this throws and catch them.
-        except timeout:
-            sender.close()
-            print("Send request timed out.")
-            return False
-        except OSError as e:
-            sender.close()
-            print(e)
-            return False
-        except Exception as e:
-            sender.close()
-            print(e)
-            return False
+
+        with open(self.get_filename(), "rb") as f:
+            data = f.read()
+
+        packet_size = self._mtu - self._header_size
+        data = [data[x: x + packet_size] for x in range(0, len(data), packet_size)]
+        packet_count = len(data)
+
+        for i, datum in enumerate(data, start = 1):
+            print(f"sending {packet_size}B of actual data.")
+            sender.sendto(self._make_header(i, packet_count, listening_ip, listening_port) + datum, self.get_receiver())
+            sleep(0.2)
+            try:
+                data, _ = sender.recvfrom(self._mtu)
+                if self._check_ack(data):
+                    print("Received Acknowledgement!")
+                    continue
+            except timeout:
+                sender.close()
+                return False
+            except OSError:
+                print("There are no valid receivers to receive this data exitting...")
+                sender.close()
+                return False
+            except Exception as e:
+                print(f"{e}")
+                sender.close()
+                return False
+            
         return True
 
-    def _send_file_sw(self):
-        pass
+
+    def _send_file_sw(self) -> bool:
+        return False
+
+    def _make_header(self, packet_number: int, packet_count: int, listening_ip: str, listening_port: int) -> bytes:
+        packet_number = packet_number.to_bytes(length = 2, signed = False)
+        packet_count = packet_count.to_bytes(length = 2, signed = False)
+        listening_ip = inet_aton(listening_ip)
+        listening_port = listening_port.to_bytes(length = 2, signed = False)
+        return packet_count + packet_number + listening_ip + listening_port
+    
+    def _check_ack(self, packet) -> bool:
+        if int.from_bytes(packet[0:4]) == 255:
+            return True
+        return False
+
+    #endregion
